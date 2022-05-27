71a4c09af1ca14c14fc81b4666efb86ec804a195
Vadim Dolbyshev <klevertpk@gmail.com>
2022-05-27 12:52:49 +0700

    HomeWork_10

---
+++ YjmeWork_10/script.js
@@ -0,0 +1,29 @@
+'use strict'
+
+// Переменные
+// Объявляем переменную
+let user;
+let ege;
+let correctValue = true;
+let invalidValue = false;
+let numberLowercase;
+
+// Присваеваем переменной значение
+user = 'Vadim';
+ege = 38;
+numberLowercase = +'123'
+
+// Константы
+
+// Объявляем константу и присваеваем сразу значение
+
+const myBirthday = '28.06.1983';
+const mySurname = 'Dolbyshev';
+const COLOR_GREEN = "#0F0"
+
+
+console.log(mySurname, ege, 'Лет');
+
+console.log(numberLowercase);
+
+console.log('1>10', invalidValue);
